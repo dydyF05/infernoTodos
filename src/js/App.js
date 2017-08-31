56a42372e0fc0d9571422f0e1bbcd57426493ccc
dydy <dylanfoster.esgi@gmail.com>
2017-08-31 18:58:48 +0200

    testing architecture

--- src/js/App.js
+++ src/js/App.js
@@ -1,8 +1,8 @@
 import { version } from 'inferno';
 import Component from 'inferno-component';
-import './registerServiceWorker';
-import Logo from './logo';
-import './App.css';
+import '../registerServiceWorker';
+import Logo from '../logo';
+import '../css/App.css';
 
 class App extends Component {
   render() {
